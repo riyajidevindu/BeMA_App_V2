f6da464068e8ba6ffe915620041bd79619bd6933
riyajidevindu <riyajidevindu@gmail.com>
2024-09-06 15:08:17 +0530

    colors

---
+++ bema_application/lib/common/config/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF0098FF);
+const Color backgroundColor = Color(0xFFE8F2FF);
+const Color textColor = Color(0xFF000000);
+const Color secondaryTextColor = Color(0xFF6A6A6A);
+const Color errorColor = Colors.red;
+const Color successColor = Colors.green;
+const Color placeholderColor = Colors.grey;
