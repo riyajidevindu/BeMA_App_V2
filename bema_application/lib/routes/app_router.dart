99c8140fc1945d997d6de7e94d7bc6569079e2fe
Sahaswari Senanayaka <smsahaswari@gmail.com>
2024-09-14 00:57:31 +0530

    Create routes for the welcome page

--- bema_application/lib/routes/app_router.dart
+++ bema_application/lib/routes/app_router.dart
@@ -1,12 +1,20 @@
 import 'package:bema_application/features/authentication/screens/login_screen.dart';
 import 'package:bema_application/features/authentication/screens/signup_screen.dart';
+import 'package:bema_application/routes/authendication_wrapper.dart';
 import 'package:bema_application/routes/route_names.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 final goRouter = GoRouter(
-  initialLocation: '/${RouteNames.registerScreen}',
+   initialLocation: '/${RouteNames.wrapper}',
   routes: [
+     GoRoute(
+      path: '/${RouteNames.wrapper}',
+      name: RouteNames.wrapper,
+      pageBuilder: (context, builder) => const MaterialPage(
+        child: AuthenticationWrapper(),
+      ),
+    ),
     GoRoute(
       path: '/${RouteNames.registerScreen}',
       name: RouteNames.registerScreen,
--- bema_application/lib/routes/route_names.dart
+++ bema_application/lib/routes/route_names.dart
@@ -1,4 +1,5 @@
 class RouteNames {
+  static const wrapper = 'wrapper';
   static const registerScreen = 'registerScreen';
   static const loginScreen = 'loginScreen';
 }
