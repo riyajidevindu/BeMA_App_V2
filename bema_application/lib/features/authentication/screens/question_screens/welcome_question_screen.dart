27a4247d79183e8e322adf3e0cb1b6076cba0fff
Sahaswari Senanayaka <smsahaswari@gmail.com>
2024-09-16 12:10:50 +0530

    Create user welcome page

---
+++ bema_application/lib/features/authentication/screens/question_screens/welcome_question_screen.dart
@@ -0,0 +1,55 @@
+import 'package:bema_application/common/widgets/snackbar%20messages/snackbar_message.dart';
+import 'package:bema_application/features/authentication/data/models/login_result.dart';
+import 'package:bema_application/features/authentication/providers/authentication_provider.dart';
+import 'package:bema_application/common/widgets/buttons/custom_elevation_buttons.dart';
+import 'package:bema_application/routes/route_names.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
+
+class UserWelcomeScreen extends StatelessWidget {
+  final String userName = "Devindu";
+
+  const UserWelcomeScreen({super.key}); // You can replace it with Firebase Auth name.
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "👋 Hi, $userName!",
+                style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                "Tell us about you!",
+                style: TextStyle(fontSize: 20),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                "Let's become friends 😜",
+                style: TextStyle(fontSize: 20),
+              ),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: () {
+                  // Handle next action
+                },
+                child: Text("Continue"),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  minimumSize: Size(double.infinity, 50),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
