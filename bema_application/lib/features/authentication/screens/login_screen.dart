6a427e5c17b28ca88060d36ce9ee918ff637cf0d
riyajidevindu <riyajidevindu@gmail.com>
2024-09-27 18:28:16 +0530

    login UI fixed

--- bema_application/lib/features/authentication/screens/login_screen.dart
+++ bema_application/lib/features/authentication/screens/login_screen.dart
@@ -68,25 +68,17 @@ class _LoginScreenState extends State<LoginScreen> {
 
     return Scaffold(
       backgroundColor: const Color(0xFFE8F2FF),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: screenWidth * 0.08,
-            vertical: screenHeight * 0.02,
-          ),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(height: screenHeight * 0.04), // Adjusted for responsiveness
+      body: Column(
+        children: [
+          SizedBox(height: screenHeight * 0.05), // Adjusted for responsiveness
 
-                // Logo at the top
-                Center(
-                  child: Image.asset(
-                    'assets/logo.png', // Ensure this path is correct
-                    height: screenHeight * 0.25, // Responsive logo size
-                  ),
+          // Logo and Login Text (Fixed elements)
+          Center(
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets/logo.png', // Ensure this path is correct
+                  height: screenHeight * 0.25, // Responsive logo size
                 ),
                 SizedBox(height: screenHeight * 0.02), // Spacing after the logo
 
@@ -99,111 +91,130 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                   textAlign: TextAlign.center,
                 ),
-                SizedBox(height: screenHeight * 0.05),
-
-                // Email Field
-                TextFormField(
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.email, color: Colors.grey[600]), // Add email icon
-                    labelText: 'Email',
-                    labelStyle: const TextStyle(color: Colors.grey),
-                    fillColor: const Color(0xFFFFFFFF),
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12), // More rounded corners
-                    ),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: screenHeight * 0.02),
-
-                // Password Field
-                TextFormField(
-                  controller: _passwordController,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.lock, color: Colors.grey[600]), // Add password icon
-                    labelText: 'Password',
-                    labelStyle: const TextStyle(color: Colors.grey),
-                    fillColor: const Color(0xFFFFFFFF),
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12), // More rounded corners
-                    ),
-                  ),
-                  obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your password';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: screenHeight * 0.04),
-
-                // Custom Elevated Button
-                CustomElevationBtn(
-                  buttonName: 'Login',
-                  onClick: _login,
-                  isSubmitting: isSubmitting,
-                ),
-                SizedBox(height: screenHeight * 0.02),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'If you don\'t have an account ',
-                      style: TextStyle(color: secondaryTextColor, fontSize: 16),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        context.goNamed(RouteNames.registerScreen);
-                      },
-                      child: const Text(
-                        'Register',
-                        style: TextStyle(
-                          color: Colors.blue, // Accent color for the clickable text
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: screenHeight * 0.01),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'Forgot your password? ',
-                      style: TextStyle(color: secondaryTextColor, fontSize: 16),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        // Add functionality to reset password
-                      },
-                      child: const Text(
-                        'Reset',
-                        style: TextStyle(
-                          color: Colors.blue, // Accent color for clickable text
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: screenHeight * 0.04), // More spacing at the end
+                SizedBox(height: screenHeight * 0.02), // Slightly reduce the space
               ],
             ),
           ),
-        ),
+
+          // Scrollable part (Email, Password, Button, etc.)
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: screenWidth * 0.08,
+                  vertical: screenHeight * 0.02,
+                ),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      // Email Field
+                      TextFormField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.email, color: Colors.grey[600]), // Add email icon
+                          labelText: 'Email',
+                          labelStyle: const TextStyle(color: Colors.grey),
+                          fillColor: const Color(0xFFFFFFFF),
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12), // More rounded corners
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your email';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(height: screenHeight * 0.02),
+
+                      // Password Field
+                      TextFormField(
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.lock, color: Colors.grey[600]), // Add password icon
+                          labelText: 'Password',
+                          labelStyle: const TextStyle(color: Colors.grey),
+                          fillColor: const Color(0xFFFFFFFF),
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12), // More rounded corners
+                          ),
+                        ),
+                        obscureText: true,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your password';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(height: screenHeight * 0.04),
+
+                      // Custom Elevated Button
+                      CustomElevationBtn(
+                        buttonName: 'Login',
+                        onClick: _login,
+                        isSubmitting: isSubmitting,
+                      ),
+                      SizedBox(height: screenHeight * 0.02),
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'If you don\'t have an account ',
+                            style: TextStyle(color: secondaryTextColor, fontSize: 16),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              context.goNamed(RouteNames.registerScreen);
+                            },
+                            child: const Text(
+                              'Register',
+                              style: TextStyle(
+                                color: Colors.blue, // Accent color for the clickable text
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: screenHeight * 0.01),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Forgot your password? ',
+                            style: TextStyle(color: secondaryTextColor, fontSize: 16),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              // Add functionality to reset password
+                            },
+                            child: const Text(
+                              'Reset',
+                              style: TextStyle(
+                                color: Colors.blue, // Accent color for clickable text
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: screenHeight * 0.04), // More spacing at the end
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
