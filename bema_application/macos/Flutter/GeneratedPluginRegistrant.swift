9ee21585a2324de558faefbb232b7529ae5d3b02
riyajidevindu <riyajidevindu@gmail.com>
2024-11-13 20:23:07 +0530

    pub changes

--- bema_application/macos/Flutter/GeneratedPluginRegistrant.swift
+++ bema_application/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,22 +5,30 @@
 import FlutterMacOS
 import Foundation
 
+import audio_session
 import cloud_firestore
 import firebase_auth
 import firebase_core
 import firebase_storage
+import just_audio
 import path_provider_foundation
+import record_darwin
 import shared_preferences_foundation
 import speech_to_text
 import sqflite
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
