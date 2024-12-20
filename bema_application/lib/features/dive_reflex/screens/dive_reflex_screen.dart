1730bf7bbde089e61185e2bf361ce0036204b0fa
Riyaji Samasundara <riyajidevindu@gmail.com>
2024-12-20 15:08:53 +0530

    Add success snackbar message and enhance DiveReflexScreen layout with responsive design

--- bema_application/lib/features/dive_reflex/screens/dive_reflex_screen.dart
+++ bema_application/lib/features/dive_reflex/screens/dive_reflex_screen.dart
@@ -1,5 +1,6 @@
 import 'package:bema_application/common/config/colors.dart';
 import 'package:bema_application/common/widgets/app_bar.dart';
+import 'package:bema_application/common/widgets/snackbar%20messages/snackbar_message.dart';
 import 'package:flutter/material.dart';
 import 'start_relax_screen.dart';
 import 'text_guide_screen.dart';
@@ -13,143 +14,166 @@ class DiveReflexScreen extends StatelessWidget {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
 
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Future.delayed(const Duration(seconds: 1), () {
+        showSuccessSnackBarMessage(
+          context,
+          "Need guidance? Check out the Guides.",
+        );
+      });
+    });
+
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         backgroundColor: backgroundColor,
         title: const CustomAppBar(showBackButton: true),
-        automaticallyImplyLeading: false
+        automaticallyImplyLeading: false,
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          // Title Section
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              "Dive Reflex Relaxation",
-              style: TextStyle(
-                fontSize: screenWidth * 0.07,
-                fontWeight: FontWeight.bold,
-                color: Colors.blueAccent,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          const SizedBox(height: 20),
-
-          // Illustration for Dive Reflex
-          Container(
-            height: screenHeight * 0.3,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/meditation.png"),
-                fit: BoxFit.contain,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(screenWidth * 0.05),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // Title Section
+            Padding(
+              padding: EdgeInsets.all(screenWidth * 0.04),
+              child: Text(
+                "Dive Reflex Relaxation",
+                style: TextStyle(
+                  fontSize: screenWidth * 0.07,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blueAccent,
+                ),
+                textAlign: TextAlign.center,
               ),
             ),
-          ),
-          const SizedBox(height: 30),
+            SizedBox(height: screenHeight * 0.02),
 
-          // Buttons Section
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Column(
-              children: [
-                // Start Relax Button
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const StartRelaxScreen(),
-                      ),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blueAccent,
-                    padding: EdgeInsets.symmetric(
-                      horizontal: screenWidth * 0.25,
-                      vertical: screenHeight * 0.02,
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25),
-                    ),
+            // Illustration for Dive Reflex
+            Container(
+              height: screenHeight * 0.3,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(screenWidth * 0.05),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: const Offset(0, 3),
                   ),
-                  child: Text(
-                    "Start Relax",
-                    style: TextStyle(
-                      fontSize: screenWidth * 0.05,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
+                ],
+                image: const DecorationImage(
+                  image: AssetImage("assets/meditation.png"),
+                  fit: BoxFit.contain,
                 ),
-                const SizedBox(height: 20),
-
-                // Video Guide Button
-                ElevatedButton.icon(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const VideoGuideScreen(),
-                      ),
-                    );
-                  },
-                  icon: const Icon(Icons.play_circle, color: Colors.white),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.orangeAccent,
-                    padding: EdgeInsets.symmetric(
-                      horizontal: screenWidth * 0.18,
-                      vertical: screenHeight * 0.015,
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25),
-                    ),
-                  ),
-                  label: Text(
-                    "Video Guide",
-                    style: TextStyle(
-                      fontSize: screenWidth * 0.05,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-
-                 ElevatedButton.icon(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const TextGuideScreen(),
-                      ),
-                    );
-                  },
-                  icon: const Icon(Icons.article, color: Colors.white),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.greenAccent,
-                    padding: EdgeInsets.symmetric(
-                      horizontal: screenWidth * 0.18,
-                      vertical: screenHeight * 0.015,
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25),
-                    ),
-                  ),
-                  label: Text(
-                    "Text Guide",
-                    style: TextStyle(
-                      fontSize: screenWidth * 0.05,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
-        ],
+            SizedBox(height: screenHeight * 0.03),
+
+            // Buttons Section
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
+              child: Column(
+                children: [
+                  // Start Relax Button
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const StartRelaxScreen(),
+                        ),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blueAccent,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: screenWidth * 0.25,
+                        vertical: screenHeight * 0.02,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                    ),
+                    child: Text(
+                      "Start Relax",
+                      style: TextStyle(
+                        fontSize: screenWidth * 0.05,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: screenHeight * 0.02),
+
+                  // Video Guide Button
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const VideoGuideScreen(),
+                        ),
+                      );
+                    },
+                    icon: const Icon(Icons.play_circle, color: Colors.white),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orangeAccent,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: screenWidth * 0.18,
+                        vertical: screenHeight * 0.015,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                    ),
+                    label: Text(
+                      "Video Guide",
+                      style: TextStyle(
+                        fontSize: screenWidth * 0.05,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: screenHeight * 0.02),
+
+                  // Text Guide Button
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const TextGuideScreen(),
+                        ),
+                      );
+                    },
+                    icon: const Icon(Icons.article, color: Colors.white),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.greenAccent,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: screenWidth * 0.18,
+                        vertical: screenHeight * 0.015,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                    ),
+                    label: Text(
+                      "Text Guide",
+                      style: TextStyle(
+                        fontSize: screenWidth * 0.05,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
