3ce0cd62610a67ce751ad1c0b6f505a6ea4f7361
Riyaji Samasundara <riyajidevindu@gmail.com>
2024-12-20 12:14:49 +0530

    Refactor TextGuideScreen to use StatefulWidget; add scroll functionality and down arrow button

--- bema_application/lib/features/dive_reflex/screens/text_guide_screen.dart
+++ bema_application/lib/features/dive_reflex/screens/text_guide_screen.dart
@@ -2,8 +2,57 @@ import 'package:bema_application/common/config/colors.dart';
 import 'package:bema_application/common/widgets/app_bar.dart';
 import 'package:flutter/material.dart';
 
-class TextGuideScreen extends StatelessWidget {
+class TextGuideScreen extends StatefulWidget {
   const TextGuideScreen({Key? key}) : super(key: key);
+
+  @override
+  _TextGuideScreenState createState() => _TextGuideScreenState();
+}
+
+class _TextGuideScreenState extends State<TextGuideScreen> {
+  final ScrollController _scrollController = ScrollController();
+  bool _showDownArrow = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_scrollListener);
+  }
+
+  void _scrollListener() {
+    if (_scrollController.position.atEdge) {
+      if (_scrollController.position.pixels == 0) {
+        // At the top
+        setState(() {
+          _showDownArrow = true;
+        });
+      } else {
+        // At the bottom
+        setState(() {
+          _showDownArrow = false;
+        });
+      }
+    } else {
+      setState(() {
+        _showDownArrow = true;
+      });
+    }
+  }
+
+  void _scrollToBottom() {
+    _scrollController.animateTo(
+      _scrollController.position.maxScrollExtent,
+      duration: const Duration(seconds: 1),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_scrollListener);
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,76 +89,97 @@ class TextGuideScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: backgroundColor,
         title: const CustomAppBar(showBackButton: true),
-        automaticallyImplyLeading: false
+        automaticallyImplyLeading: false,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            // Title Section
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                "Text Guide",
-                style: TextStyle(
-                  fontSize: screenWidth * 0.07,
-                  fontWeight: FontWeight.bold,
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            controller: _scrollController,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // Title Section
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    "Text Guide",
+                    style: TextStyle(
+                      fontSize: screenWidth * 0.07,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueAccent,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const SizedBox(height: 20),
+
+                // Steps Section
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Column(
+                    children: steps.map((step) {
+                      int index = steps.indexOf(step);
+                      return Card(
+                        margin: const EdgeInsets.symmetric(vertical: 8.0),
+                        elevation: 4,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: ListTile(
+                          leading: CircleAvatar(
+                            backgroundColor: Colors.blueAccent,
+                            child: Text(
+                              "${index + 1}",
+                              style: const TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          title: RichText(
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: "${step['heading']}\n", // Added extra newline for more space
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                    height: 2.0, 
+                                  ),
+                                ),
+                                TextSpan(
+                                  text: step['content'],
+                                  style: const TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.normal,
+                                    color: Colors.black,
+                                    height: 1.4,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          if (_showDownArrow)
+            Positioned(
+              bottom: 16,
+              right: 16,
+              child: FloatingActionButton(
+                onPressed: _scrollToBottom,
+                backgroundColor: Colors.white.withOpacity(0.8),
+                elevation: 4,
+                child: const Icon(
+                  Icons.arrow_downward,
                   color: Colors.blueAccent,
                 ),
-                textAlign: TextAlign.center,
               ),
             ),
-            const SizedBox(height: 20),
-
-            // Steps Section
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Column(
-                children: steps.map((step) {
-                  int index = steps.indexOf(step);
-                  return Card(
-                    margin: const EdgeInsets.symmetric(vertical: 8.0),
-                    elevation: 4,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        backgroundColor: Colors.blueAccent,
-                        child: Text(
-                          "${index + 1}",
-                          style: const TextStyle(color: Colors.white),
-                        ),
-                      ),
-                      title: RichText(
-                        text: TextSpan(
-                          children: [
-                            TextSpan(
-                              text: "${step['heading']}\n",
-                              style: const TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
-                              ),
-                            ),
-                            TextSpan(
-                              text: step['content'],
-                              style: const TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.normal,
-                                color: Colors.black,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                }).toList(),
-              ),
-            ),
-          ],
-        ),
+        ],
       ),
     );
   }
