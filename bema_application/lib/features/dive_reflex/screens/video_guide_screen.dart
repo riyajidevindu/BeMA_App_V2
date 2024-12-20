5695b95b162e6844f462d4cffd13f5e64b4abe89
Riyaji Samasundara <riyajidevindu@gmail.com>
2024-12-20 13:04:13 +0530

    Implement VideoGuideScreen with video playback functionality and controls

--- bema_application/lib/features/dive_reflex/screens/video_guide_screen.dart
+++ bema_application/lib/features/dive_reflex/screens/video_guide_screen.dart
@@ -1,27 +1,180 @@
 import 'package:bema_application/common/config/colors.dart';
 import 'package:bema_application/common/widgets/app_bar.dart';
+import 'package:bema_application/common/widgets/progress_indicator/custom_progress_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class VideoGuideScreen extends StatelessWidget {
+class VideoGuideScreen extends StatefulWidget {
   const VideoGuideScreen({Key? key}) : super(key: key);
 
   @override
+  _VideoGuideScreenState createState() => _VideoGuideScreenState();
+}
+
+class _VideoGuideScreenState extends State<VideoGuideScreen> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset('assets/guide_video.mp4')
+      ..initialize().then((_) {
+        setState(() {}); // Ensure the first frame is shown after the video is initialized
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _rewind() {
+    final currentPosition = _controller.value.position;
+    final newPosition = currentPosition - const Duration(seconds: 5);
+    _controller.seekTo(newPosition);
+  }
+
+  void _forward() {
+    final currentPosition = _controller.value.position;
+    final newPosition = currentPosition + const Duration(seconds: 5);
+    _controller.seekTo(newPosition);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: backgroundColor,
         title: const CustomAppBar(showBackButton: true),
-        automaticallyImplyLeading: false
+        automaticallyImplyLeading: false,
       ),
       body: Center(
-        child: Text(
-          "Video Player Integration Goes Here!",
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            color: Colors.blueGrey,
-          ),
-          textAlign: TextAlign.center,
+        child: _controller.value.isInitialized
+            ? Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // Heading
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        "Video Guide",
+                        style: TextStyle(
+                          fontSize: screenWidth * 0.07,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blueAccent,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+
+                    // Video Frame
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(color: Colors.white, width: 2),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.2),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: AspectRatio(
+                          aspectRatio: _controller.value.aspectRatio,
+                          child: VideoPlayer(_controller),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+
+                    // Video Progress Indicator
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: VideoProgressIndicator(
+                        _controller,
+                        allowScrubbing: true,
+                        colors: VideoProgressColors(
+                          playedColor: Colors.white,
+                          backgroundColor: Colors.grey,
+                          bufferedColor: Colors.lightBlueAccent,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+
+                    // Action Buttons Panel
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.2),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      padding: const EdgeInsets.all(16),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          _buildActionButton(
+                            icon: Icons.replay_10,
+                            onPressed: _rewind,
+                          ),
+                          _buildActionButton(
+                            icon: _controller.value.isPlaying
+                                ? Icons.pause
+                                : Icons.play_arrow,
+                            onPressed: () {
+                              setState(() {
+                                _controller.value.isPlaying
+                                    ? _controller.pause()
+                                    : _controller.play();
+                              });
+                            },
+                          ),
+                          _buildActionButton(
+                            icon: Icons.forward_10,
+                            onPressed: _forward,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            : const CustomProgressIndicator(),
+      ),
+    );
+  }
+
+  Widget _buildActionButton({required IconData icon, required VoidCallback onPressed}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          shape: const CircleBorder(),
+          padding: const EdgeInsets.all(16),
+          elevation: 10,
+          shadowColor: Colors.black.withOpacity(0.5),
+        ),
+        child: Icon(
+          icon,
+          size: 25
         ),
       ),
     );
